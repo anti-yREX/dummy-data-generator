c2f13f642a14dd8c2ec20b0e7b180be07303ae33
Rituraj Ingale <ritu.raj.ingale@gmail.com>
2022-12-04 11:40:10 +0530

    Initial commit with React App setup

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./src/App"
+
+const rootEle = document.getElementById("root");
+
+const root = createRoot(rootEle);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
