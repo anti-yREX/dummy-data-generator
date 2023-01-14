6c9b1d78c414fda7ec62454bdcd3e09b8cff169a
Rituraj Ingale <ritu.raj.ingale@gmail.com>
2023-01-14 14:53:03 +0530

    Added Deletion functionality

--- src/app/ObjectListPanel/ObjectListPanel.jsx
+++ src/app/ObjectListPanel/ObjectListPanel.jsx
@@ -3,6 +3,7 @@ import { Add, Delete, Redo, Undo } from '@mui/icons-material';
 import { useSelector, useDispatch, batch } from 'react-redux';
 import { setNewKeyFieldData } from '../../services/reduxStore/NewKeyFieldReducer';
 import { setSelectedKey } from '../../services/reduxStore/SelectedKeyReducer';
+import { deleteKeyFromParentObj } from '../../services/reduxStore/ParentObjectStateReducer';
 import {
     Wrapper,
     Container,
@@ -14,9 +15,18 @@ import KeysList from './components/KeysList/KeysList';
 const objectListContainerId = 'object-list-container';
 
 const ObjectListPanel = () => {
-    const {selectedKey, currentPath} = useSelector(
-        ({ selectedKey, newKeyFieldState }) => ({selectedKey, currentPath: newKeyFieldState.path})
+    const {
+        selectedKey,
+        currentPath,
+        parentObject,
+    } = useSelector(
+        ({ selectedKey, newKeyFieldState, parentObject }) => ({
+            selectedKey,
+            currentPath: newKeyFieldState.path,
+            parentObject,
+        })
     );
+
     const dispatch = useDispatch();
     const selected = selectedKey === null || selectedKey?.path?.length === 0;
 
@@ -40,6 +50,68 @@ const ObjectListPanel = () => {
             });
         }
     }
+
+    const getSiblingIndex = (parentObject, targetKey) => {
+        let siblingIndex = -1;
+        const keyList = Object.keys(parentObject);
+        for (let i = 0; i < keyList.length; i++) {
+            if (keyList[i] === targetKey) {
+                if (i === keyList.length - 1) {
+                    siblingIndex = i - 1;
+                } else {
+                    siblingIndex = i + 1;
+                }
+            }
+        }
+        return {
+            siblingIndex,
+            keyName: keyList[siblingIndex],
+        };
+    }
+
+    const onDeleteHandler = () => {
+        const targetPath = selectedKey?.path;
+        let newSelectedKey = { path: [] };
+        if (targetPath.length > 0) {
+            if (targetPath.length === 1) {
+                const targetKey = targetPath[0];
+                const { siblingIndex, keyName } = getSiblingIndex(parentObject, targetKey);
+                if (siblingIndex !== -1) {
+                    newSelectedKey = parentObject[keyName];
+                }
+            } else {
+                let targetParent = parentObject[targetPath[0]];
+                for(let i = 1; i < targetPath.length - 1; i++) {
+                    targetParent = targetParent.children[targetPath[i]];
+                }
+                const targetKey = targetPath[targetPath.length - 1];
+                const { siblingIndex, keyName } = getSiblingIndex(targetParent.children, targetKey);
+                if (siblingIndex === -1) {
+                    newSelectedKey = {
+                        path: targetParent.path,
+                        keyName: targetParent.keyName,
+                        type: targetParent.type,
+                    };
+                } else {
+                    const sibling = targetParent.children[keyName];
+                    newSelectedKey = {
+                        path: sibling.path,
+                        keyName: sibling.keyName,
+                        type: sibling.type,
+                    };
+                }
+            }
+        }
+        batch(() => {
+            dispatch(
+                setSelectedKey(newSelectedKey)
+            );
+            dispatch(
+                deleteKeyFromParentObj(targetPath)
+            );
+        });
+    }
+
     return (
         <Wrapper>
             <ButtonWrapper>
@@ -48,14 +120,11 @@ const ObjectListPanel = () => {
                 >
                     <Add />
                 </StyledIconButton>
-                <StyledIconButton disabled >
+                <StyledIconButton
+                    disabled={selected}
+                    onClick={onDeleteHandler}
+                >
                     <Delete />
-                </StyledIconButton>
-                <StyledIconButton className="right-side" disabled >
-                    <Undo />
-                </StyledIconButton>
-                <StyledIconButton disabled >
-                    <Redo />
                 </StyledIconButton>
             </ButtonWrapper>
             <Container
--- src/services/reduxStore/ParentObjectStateReducer/index.js
+++ src/services/reduxStore/ParentObjectStateReducer/index.js
@@ -68,10 +68,24 @@ export const parentObjectSlice = createSlice({
                 ...state,
                 ...action.payload,
             };
+        },
+        deleteKeyFromParentObj: (state, action) => {
+            const path = action.payload;
+            if (path?.length > 0) {
+                if (path.length === 1) {
+                    delete state[path[0]];
+                    return;
+                }
+                let lastObj = state[path[0]];
+                for(let i = 1; i < path.length - 1; i++) {
+                    lastObj = lastObj.children[path[i]];
+                }
+                delete lastObj.children[path[path.length - 1]];
+            }
         }
     },
 });
 
-export const { setParentObjectData } = parentObjectSlice.actions;
+export const { setParentObjectData, deleteKeyFromParentObj } = parentObjectSlice.actions;
 
 export default parentObjectSlice.reducer;
